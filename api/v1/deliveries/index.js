3bd8debe513ab66f686f51baf9b9a1ddd038846c
Gustavo Porto Guedes <gustavo.guedes@fatec.sp.gov.br>
2019-01-18 07:35:14 -0200

    Add first implementation of our API routes :heavy_plus_sign:
    This commit implements all endpoints planned for our API
    V1.
    
    Signed-of-by: Gustavo Porto Guedes <gustavo.guedes@fatec.sp.gov.br>

---
+++ api/v1/deliveries/index.js
@@ -0,0 +1,14 @@
+const create = require('./create');
+const getByDate = require('./getByDate');
+const getById = require('./getById');
+const update = require('./update');
+
+const deliveries = require('express').Router();
+
+deliveries.get('/:id', getById);
+deliveries.get('/:date_from/:date_to', getByDate);
+deliveries.post('/', create);
+deliveries.put('/', update);
+
+module.exports = deliveries;
+
