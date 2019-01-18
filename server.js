82985c81e9a97b9bba53e5c2a33455a4c9f7b7be
Gustavo Porto Guedes <gportog@br.ibm.com>
2019-01-18 07:35:14 -0200

    Add first implementation of our API routes :heavy_plus_sign:
    This commit implements all endpoints planned for our API
    V1.
    
    Signed-of-by: Gustavo Porto Guedes <gportog@br.ibm.com>

---
+++ server.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+const express = require('express'),
+    api = require('./api'),
+    bodyParser = require('body-parser'),
+    morgan = require('morgan');
+    
+const app = express();
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+// Configuring API routes
+app.use('/api', api);
+
+app.listen(process.env.PORT, () => 
+    console.log(`Server listening at port ${process.env.PORT}`)
+);
