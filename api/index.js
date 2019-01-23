5f47a642cf30a72f94931fd588237b7ed83dca2a
Gustavo Porto Guedes <gustavo.guedes@fatec.sp.gov.br>
2019-01-23 07:43:53 -0200

    Add error handler for API routes :heavy_plus_sign:
    This commit implements a basic error handler for our API,
    whatenever error passed in the next() will be processed by
    this handler.
    
    Signed-of-by: Gustavo Porto Guedes <gustavo.guedes@fatec.sp.gov.br>

--- api/index.js
+++ api/index.js
@@ -1,7 +1,18 @@
 const routes = require('express').Router();
 const v1 = require('./v1');
 
+const HTTPstatusCodes = require('./lib/HTTPstatusCodes');
+const ErrorMessages = require('./lib/ErrorMessages');
+
 routes.use('/v1', v1);
 // Later on we may have other API versions, that will be added here.
+
+// Error Handler for API
+routes.use(function (err, req, res, next) {
+    return res.status(err.status || HTTPstatusCodes.INTERNAL_SERVER_ERROR).json({
+        success: false,
+        message: err.message
+    });
+});
 
 module.exports = routes;
