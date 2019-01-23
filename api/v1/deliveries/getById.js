e12bbdd6488b4fc567af8017093ba9f784245b27
Gustavo Porto Guedes <gustavo.guedes@fatec.sp.gov.br>
2019-01-23 07:48:31 -0200

    Add getById route :heavy_plus_sign:
    This route tries to find a document by its id in our 'deliveries' db.
    If it finds something, it returns the metadata, otherwise it throws an
    error.
    
    Signed-of-by: Gustavo Porto Guedes <gustavo.guedes@fatec.sp.gov.br>

--- api/v1/deliveries/getById.js
+++ api/v1/deliveries/getById.js
@@ -1,3 +1,26 @@
-module.exports = (req, res) => {
-    res.send('GetById endpoint');
-}
+const DBClient = require('../../lib/DBClient');
+const HTTPstatusCodes = require('../../lib/HTTPstatusCodes');
+const ErrorMessages = require('../../lib/ErrorMessages');
+
+const dbInstance = new DBClient('deliveries');
+
+module.exports = (req, res, next) => {
+    dbInstance.getById(req.params.id)
+        .then((resp) => {
+            res.status(HTTPstatusCodes.OK).json({
+                    success: true,
+                    message: "The delivery was successfuly retrieved.",
+                    data: resp
+                })
+        })
+        .catch((err) => {
+            console.log(err.message);
+            if (err.message === 'missing') {
+                err = new Error(ErrorMessages.DB_DOC_NOT_FOUND(req.params.id));
+                err.status = HTTPstatusCodes.NOT_FOUND;
+            } else {
+                err = new Error(ErrorMessages.INTERNAL_SERVER_ERROR());
+            }
+            return next(err)
+        })
+};
