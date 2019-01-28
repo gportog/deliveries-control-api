32a075596f008a1bf366c012b9854a82dffacf5d
Gustavo Porto Guedes <gportog@br.ibm.com>
2019-01-28 16:47:21 -0200

    Add getByDate route :heavy_plus_sign:
    This route checks if :date_from and :date_to params are valid,
    if :date_from is higther than :date_to it throws an error,
    otherwise it queries the DB.
    
    Signed-of-by: Gustavo Porto Guedes <gportog@br.ibm.com>

--- api/v1/deliveries/getByDate.js
+++ api/v1/deliveries/getByDate.js
@@ -1,3 +1,49 @@
-module.exports = (req, res) => {
-    res.send('Date endpoint');
-}
+const DBClient = require('../../lib/DBClient');
+const HTTPstatusCodes = require('../../lib/HTTPstatusCodes');
+const ErrorMessages = require('../../lib/ErrorMessages');
+
+const dbInstance = new DBClient('deliveries');
+
+module.exports = (req, res, next) => {
+    if (new Date(req.params.date_from) == 'Invalid Date' || req.params.date_from.search('[0-9]{4}-[0-9]{2}-[0-9]{2}') !== 0) {
+        let err = new Error(ErrorMessages.TYPE_MISMATCH_ERROR('date_from', 'date(YYYY-MM-DD)', new Date(req.params.date_from)));
+        err.status = HTTPstatusCodes.BAD_REQUEST;
+        return next(err);
+    }
+    if (new Date(req.params.date_to) == 'Invalid Date' || req.params.date_to.search('[0-9]{4}-[0-9]{2}-[0-9]{2}') !== 0) {
+        let err = new Error(ErrorMessages.TYPE_MISMATCH_ERROR('date_to', 'date(YYYY-MM-DD)', new Date(req.params.date_to)));
+        err.status = HTTPstatusCodes.BAD_REQUEST;
+        return next(err);
+    }
+    if (new Date(req.params.date_from) > new Date(req.params.date_to)) {
+        let err = new Error(ErrorMessages.VALUE_MISMATCH_ERROR('date_from', 'less than date_to', req.params.date_from));
+        err.status = HTTPstatusCodes.BAD_REQUEST;
+        return next(err);
+    }
+
+    dbInstance.search({
+        selector: {
+            deliveryman: req.params.deliveryman,
+            date: {
+                $gte: req.params.date_from,
+                $lte: req.params.date_to
+            }
+        }
+    })
+        .then((resp) => {
+            if (resp.docs.length === 0) throw new Error(ErrorMessages.DATA_NOT_FOUND());
+            return res.status(HTTPstatusCodes.OK).json({
+                success: true,
+                message: "The deliveries was successfuly retrieved.",
+                data: resp
+            })
+        })
+        .catch((err) => {
+            if (err.message === ErrorMessages.DATA_NOT_FOUND()) {
+                err.status = HTTPstatusCodes.NOT_FOUND;
+            } else {
+                err = new Error(ErrorMessages.INTERNAL_SERVER_ERROR());
+            }
+            return next(err)
+        })
+};
