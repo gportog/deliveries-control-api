7717255b0a33943173ee37059fc2c963b711d34d
Gustavo Porto Guedes <gustavo.guedes@fatec.sp.gov.br>
2019-01-30 11:29:06 -0200

    Add basic authentication to our API :heavy_plus_sign:
    This commit implements a middleware for our API to
    authenticate users, using passport.js basic strategy.
    
    Signed-of-by: Gustavo Porto Guedes <gustavo.guedes@fatec.sp.gov.br>

--- server.js
+++ server.js
@@ -3,14 +3,17 @@ const express = require('express'),
     api = require('./api'),
     bodyParser = require('body-parser'),
     morgan = require('morgan');
-    
+
+const BasicAuth = require('./auth/basic'),
+      basicAuthInstance = new BasicAuth();
+
 const app = express();
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 // Configuring API routes
-app.use('/api', api);
+app.use('/api', basicAuthInstance.basic(), api);
 
 app.listen(process.env.PORT, () => 
     console.log(`Server listening at port ${process.env.PORT}`)
